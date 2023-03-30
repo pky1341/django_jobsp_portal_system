5998d99196af01b67333a7f0fbf1c12e562593d7
pradeep kumar <pky463775@gmail.com>
2023-03-31 00:27:16 +0530

    second commit

--- jps_app/jpsurls.py
+++ jps_app/jpsurls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from jps_app import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('/find_job',views.find_job,name="find_job")
 ]
